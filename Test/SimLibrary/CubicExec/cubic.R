b7703408621e2d3b1298d449c01954f65e78e951
Alex Hogg <the.hoglet@gmail.com>
2022-07-09 17:45:22 +0100

    config serialization unit test fail

--- Test/SimLibrary/CubicExec/cubic.R
+++ Test/SimLibrary/CubicExec/cubic.R
@@ -1,24 +1,20 @@
-do_cubic <- function(params) {
+assign_parameters <- function() {
+  # BEGIN PARAMETERS
+  a <- 3 # []
+  b <- -12 # []
+  c <- 0 # []
+  d <- 50 # []
+  # END PARAMETERS
+}
+
+run_model <- function(params) {
   a <- params[["a"]]
   b <- params[["b"]]
   c <- params[["c"]]
   d <- params[["d"]]
 
-  x <- params[["x"]]
-
-  y <- a * x ^ 3 + b * x ^ 2 + c * x + d
-
-  res <- list(x = x, y = y)
-
-  return(res)
+  # BEGIN OUTPUTS
+  x <<- seq(0, 4, by = 0.4) # []
+  y <<- a * x ^ 3 + b * x ^ 2 + c * x + d # []
+  # END OUTPUTS
 }
-
-params <- list(
-  a = 3,
-  b = -12,
-  c = 0,
-  d = 50,
-  x = seq(0, 4, by = 0.4)
-  )
-
-res <- do_cubic(params)
